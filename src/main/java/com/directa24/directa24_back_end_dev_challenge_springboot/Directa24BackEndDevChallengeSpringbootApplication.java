ee54a48757184cf5103b8c00b1adaed7ed5d096c
Geisser Morillo <gssrmorillo5@gmail.com>
2024-12-16 00:18:53 -0300

    Initial Commit

---
+++ src/main/java/com/directa24/directa24_back_end_dev_challenge_springboot/Directa24BackEndDevChallengeSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.directa24.directa24_back_end_dev_challenge_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Directa24BackEndDevChallengeSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Directa24BackEndDevChallengeSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/directa24/directa24_back_end_dev_challenge_springboot/Directa24BackEndDevChallengeSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.directa24.directa24_back_end_dev_challenge_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Directa24BackEndDevChallengeSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
